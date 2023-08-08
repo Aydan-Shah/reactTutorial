101d60589b18139ec78857faf823c1d9e0a4e095
Aydan <aydanhilal5@gamil.com>
2023-08-08 12:23:48 +0400

    component yaratmaq ve import etmek

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react'
 import './App.css';
+import User from "./components/User";
 
-export default class App extends Component {
+ class App extends Component {
   render() {
     const test = 34;
     const isAuth = true;
     return (
       <div className='App'>
-        <h1>Merhaba React</h1>
+        {/* <h1>Merhaba React</h1>
         <h4> { 1+ 1 } </h4> 
         <h4> { "test".toLocaleUpperCase() } </h4>
         <h4> { test } </h4>
-        <h4>
-          {
-            isAuth ? <p >Musbet </p> : null
-          }
-        </h4>
+        <h4> {isAuth ? <p >Musbet </p> : null} </h4> */}
+      
+
+        <h4> App Component</h4>
+        <User/>
+        <User/>
+        <User/>
       </div>
     )
   }
 }
 
+export default App;
