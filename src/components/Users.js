fff7d5d9f79196b92a127a0e6a9c7683339c8561
Aydan <aydanhilal5@gamil.com>
2023-08-10 16:26:05 +0400

    Context Api

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react'
 import User from './User'
+import UserConsumer from '../context'
 
 class Users extends Component {
   render() {
-    const {users} = this.props
     return (
-      <div>
+      <UserConsumer>
         {
-        // console.log(users)
-            users.map(user => {
-                return (
-                    <User 
-                    key = {user.id}
-                    name = {user.name}
-                    salary = {user.salary}
-                    departmen = {user.departmen}
-                    />
-                )
-            })
+          value => {
+            const {users} = value;
+             return (
+              <div>
+                {
+                // console.log(users)
+                    users.map(user => {
+                        return (
+                            <User 
+                            key = {user.id}
+                            name = {user.name}
+                            salary = {user.salary}
+                            departmen = {user.departmen}
+                            />
+                        )
+                    })
+                }
+              </div>
+            )
+          }
         }
-      </div>
+      </UserConsumer>
     )
+   
   }
 }
 export default Users;
